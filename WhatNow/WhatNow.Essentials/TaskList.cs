b4a10aff0beeac8e944fa3e68401d8a165e3f69b
MirekVales <mirekvales@users.noreply.github.com>
2018-05-23 21:23:03 +0200

    TaskList

---
+++ WhatNow/WhatNow.Essentials/TaskList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Helios.Concurrency;
+
+namespace WhatNow.Essentials
+{
+    public class TaskList : IDisposable
+    {
+        readonly CancellationTokenSource cancellationTokenSource;
+        readonly List<Task> tasks;
+
+        readonly DedicatedThreadPool pool;
+        readonly DedicatedThreadPoolTaskScheduler scheduler;
+        readonly TaskFactory taskFactory;
+
+        public int TasksCount
+            => tasks.Count;
+
+        public TaskList(int maxDegreeOfParallelism)
+        {
+            pool = new DedicatedThreadPool(new DedicatedThreadPoolSettings(maxDegreeOfParallelism));
+            scheduler = new DedicatedThreadPoolTaskScheduler(pool);
+            taskFactory = new TaskFactory(scheduler);
+            
+            cancellationTokenSource = new CancellationTokenSource();
+            tasks = new List<Task>();
+        }
+
+        public TaskList With(Action action)
+        {
+            tasks.Add(taskFactory.StartNew(action, cancellationTokenSource.Token));
+            return this;
+        }
+
+        public void Cancel()
+        {
+            cancellationTokenSource.Cancel();
+            pool.Dispose();
+        }
+
+        public void WaitAllFinished()
+            => Task.WaitAll(tasks.ToArray());
+
+        public void Dispose()
+        {
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+        }
+    }
+}
