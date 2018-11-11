85d4ad496d8b281a08642ef870ecce535653c52a
Mirek Vales <MirekVales@users.noreply.github.com>
2018-11-11 14:39:46 +0100

    Visualization

---
+++ WhatNow/WhatNow.Visualization/IActionPipeMapVisualizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using WhatNow.Contracts;
+
+namespace WhatNow.Visualization
+{
+    public interface IActionPipeMapVisualizer
+    {
+        string Visualize(IActionPipeMap map);
+
+        string Visualize(IActionPipeMap map, Type beginAt);
+    }
+}
---
+++ WhatNow/WhatNow.Visualization/Mermaid/MermaidVisualizer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WhatNow.Contracts;
+
+namespace WhatNow.Visualization.Mermaid
+{
+    public class MermaidVisualizer : IActionPipeMapVisualizer
+    {
+        const string Header = "graph TD";
+
+        public string Visualize(IActionPipeMap map)
+            => Visualize(map, map.GetEntryPoints());
+
+        public string Visualize(IActionPipeMap map, Type beginAt)
+            => Visualize(map, new[] { beginAt });
+
+        public string Visualize(IActionPipeMap map, IEnumerable<Type> entryPoints)
+        {
+            var buffer = new StringBuilder();
+            buffer.AppendLine(Header);
+
+            int label = 1;
+            int labelize() => label++;
+
+            foreach (var currentNode in entryPoints)
+            {
+                buffer.AppendLine($"subgraph {currentNode.Name}");
+                Generate(map, buffer, labelize, currentNode, labelize());
+                buffer.AppendLine("end");
+            }
+
+            return buffer.ToString();
+        }
+
+        void Generate(IActionPipeMap map, StringBuilder buffer, Func<int> labelize, Type currentNode, int currentLabel)
+        {
+            foreach (var nextNode in map.GetNext(currentNode))
+            {
+                var nextNodeLabel = labelize();
+                var token = GetTokenName(currentNode);
+                buffer.AppendLine($"{currentLabel}[{currentNode.Name}] --> |{token}| {nextNodeLabel}[{nextNode.Name}]");
+                Generate(map, buffer, labelize, nextNode, nextNodeLabel);
+            }
+        }
+
+        string GetTokenName(Type type)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition().Name.Split('`').First() == "ActionBase")
+            {
+                return type.GetGenericArguments().Last().Name;
+            }
+
+            return GetTokenName(type.BaseType);
+        }
+    }
+}
